e5dc6c24fa24d2cd94ac63f85c35bb2c1eb16480
Arthur Ruffatto <arthurruffatto@hotmail.com>
2024-10-31 16:20:17 +0000

    Get user use case

---
+++ Portfolio.Application/DTOs/User/GetUserDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Application.DTOs.User
+{
+    public class GetUserDTO
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+}
---
+++ Portfolio.Application/UseCases/User/GetUser.cs
@@ -0,0 +1,21 @@
+﻿using Portfolio.Application.DTOs.User;
+using Portfolio.Application.Interfaces;
+
+namespace Portfolio.Application.UseCases.User
+{
+    public class GetUser
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUser(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserModelOutput> ExecuteAsync(GetUserDTO getUserDTO)
+        {
+            var user = await _userRepository.GetByIdAsync(getUserDTO.Id);
+            return UserModelOutput.FromUser(user);
+        }
+    }
+}
