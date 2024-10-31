18f877156beefe0278c8ef3f6c123e551979ddb3
Arthur Ruffatto <arthurruffatto@hotmail.com>
2024-10-31 13:01:14 +0000

    create user use case

---
+++ Portfolio.Application/DTOs/User/CreateUserDTO.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Portfolio.Application.DTOs.User
+{
+    public class CreateUserDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set; }
+
+        /*public CreateUserDTO(string firstName, string lastName, string email, string bio)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Bio = bio;
+        }*/
+    }
+}
---
+++ Portfolio.Application/UseCases/User/CreateUser.cs
@@ -0,0 +1,38 @@
+﻿using Portfolio.Application.Interfaces;
+using UserEntity = Portfolio.Domain.Entities;
+using Portfolio.Domain.ValueObjects;
+using Portfolio.Application.DTOs.User;
+
+namespace Portfolio.Application.UseCases.User
+{
+    public class CreateUser
+    {
+        private readonly IUserRepository _userRepository;
+
+        public CreateUser(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<CreateUserDTO> ExecuteAsync(CreateUserDTO createUserDTO)
+        {
+            var user = new UserEntity.User
+            {
+                Id = Guid.NewGuid(),
+                Name = new FullName(createUserDTO.FirstName, createUserDTO.LastName),
+                Email = createUserDTO.Email,
+                Bio = createUserDTO.Bio
+            };
+
+            await _userRepository.AddAsync(user);
+
+            return new CreateUserDTO
+            {
+                FirstName = user.Name.FirstName,
+                LastName = user.Name.LastName,
+                Email = user.Email,
+                Bio = user.Bio
+            };
+        }
+    }
+}
