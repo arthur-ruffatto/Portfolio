58d66d6fbd2aca846b7eacb80b882537f5ae8a46
Arthur Ruffatto <arthurruffatto@hotmail.com>
2024-10-31 13:01:30 +0000

    create user use case unit test

---
+++ Portfolio.UnitTests/Application/UseCases/User/CreateUser/CreateUserTest.cs
@@ -0,0 +1,50 @@
+﻿using Bogus;
+using Moq;
+using Portfolio.Application.DTOs.User;
+using Portfolio.Application.Interfaces;
+using UserEntity = Portfolio.Domain.Entities;
+using UseCase = Portfolio.Application.UseCases.User;
+using FluentAssertions;
+
+
+namespace Portfolio.UnitTests.Application.UseCases.User.CreateUser
+{
+    public class CreateUserTest
+    {
+        private readonly Faker _faker;
+
+        public CreateUserTest()
+        {
+            _faker = new Faker();
+        }
+
+        [Fact(DisplayName = nameof(CreateUser_ShouldBeValid))]
+        [Trait("Application", "Use Cases - User")]
+        public async void CreateUser_ShouldBeValid()
+        {
+            var repositoryMock = new Mock<IUserRepository>();
+            var useCase = new UseCase.CreateUser(repositoryMock.Object);
+            var input = CreateUserDTO();
+
+            var output = await useCase.ExecuteAsync(input);
+
+            repositoryMock.Verify(repository => repository.AddAsync(It.IsAny<UserEntity.User>()), Times.Once);
+            output.Should().NotBeNull();
+            output.FirstName.Should().Be(input.FirstName);
+            output.LastName.Should().Be(input.LastName);
+            output.Email.Should().Be(input.Email);
+            output.Bio.Should().Be(input.Bio);
+        }
+
+        private CreateUserDTO CreateUserDTO() 
+        {
+            return new CreateUserDTO
+            {
+                FirstName = _faker.Name.FirstName(),
+                LastName = _faker.Name.LastName(),
+                Email = _faker.Internet.Email(),
+                Bio = _faker.Lorem.Text()
+            };
+        }
+    }
+}
